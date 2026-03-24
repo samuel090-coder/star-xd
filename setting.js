45a2db533c4d41abb5b8e6856fa463e4c70beb78
samuel090-coder <samuelsunday09066423764@gmail.com>
2026-03-24 16:01:09 +0100

    Update setting.js

--- setting.js
+++ setting.js
@@ -1,13 +1,13 @@
 //=====💫 sᴛᴀʀ  xᴅ🔥====
 
 module.exports = {
-  BOT_NAME: 'sᴛᴀʀ xᴅ', // ur bot name
-  OWNER_NAME: 'ᴍᴀʟᴠɪɴ ᴋɪɴɢ', //your name
-  OWNER_NUMBER: '263714757857', // add your phone number
+  BOT_NAME: 'sammyfx', // ur bot name
+  OWNER_NAME: 'sammyfx', //your name
+  OWNER_NUMBER: '2349066423764', // add your phone number
   PREFIXES: ['.', '!', '#'], // Multiple prefix support you can add one or more
   
   
-  SESSION_ID: '', // add your session ID
+  SESSION_ID: '', /king~OnhyCv/ add your session ID
   PAIRING_NUMBER: '', // In Case if you don't have session id get pairing code by putting your number here
   TIMEZONE: 'Africa/Harare' // ur timezone
 };
